de5e9084b8c1d1c9a35a3f7741dbdf505bef19f5
eranh20 <64043012+eranh20@users.noreply.github.com>
2022-11-16 14:32:34 +0200

    Update Status_Check.sh

--- Status_Check.sh
+++ Status_Check.sh
@@ -3,9 +3,8 @@
 # Location of Script: /etc/init.d/
 # Add to Ubuntu startup : sudo crontab -e ---> @reboot /etc/init.d/Script_Name.sh
 
-# Enter BOT{ID}:{Token}.
-# Enter chat_id={-Number}.
-# Enter the node_file NAME & fix the command line as needed(grep/cut/sed etc'. 
+# Update BOT{ID}:{Token}.
+# Update node_file NAME & fix the command line as needed(grep/cut/sed etc'). 
 
 
 
@@ -26,38 +25,38 @@ do
         then
         	if [ $counter -lt "3" ]
         	then
-        		  counter=$(( counter+1 ))
-        		  date=$(date)
-              uptime=$(uptime)
-              curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$node_stat"
-              curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$date"
-			        curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$uptime"
-			        sleep 2m
-		      else
-			        counter=$(( counter+1 ))
-			        sleep 3m
+        		counter=$(( counter+1 ))
+        		date=$(date)
+              		uptime=$(uptime)
+              		curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$node_stat"
+              		curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$date"
+		        curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$uptime"
+		        sleep 2m
+		else
+			counter=$(( counter+1 ))
+			sleep 3m
 			
-			        if [[ $counter -eq 57 ]]
-			        then
-				            counter=0
-
-			        fi
-			
-		
-		      fi
-        
-        else
-		        if [[ $counter != 0 ]]
-		        then
-			
-			            curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$node_stat"
-               		curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$date"
-			            curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$uptime"
-			            counter=0
+			if [[ $counter -eq 57 ]]
+			then
+			       counter=0
 
 		        fi
-		        counter=0
-		        sleep 1m
-	      fi
+			
+		
+		fi
+        
+        else
+		    if [[ $counter != 0 ]]
+		    then
+			
+			    curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$node_stat"
+               		    curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$date"
+			    curl -s "https://api.telegram.org/BOT{ID}:{Token}/sendMessage?chat_id={}&text=$uptime"
+			    counter=0
+
+		     fi
+		     counter=0
+		     sleep 1m
+	fi
 done
 
